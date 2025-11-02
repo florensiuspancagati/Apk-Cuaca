4872503c0870cac0b4dd7ab93ef4479a5ab44900
florensiuspancagati <florensiuspancagati@gmail.com>
2025-11-02 14:06:51 +0700

    adapt to deploy in vercel

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherform.addEventListener('submit', (e) => {
     pesanSatu.textContent = 'Sedang mencari lokasi ..'
     pesanDua.textContent = ''
 
-    fetch('http://localhost:4000/infocuaca?address='+location)
+    fetch('/infocuaca?address='+location)
     .then((response)=>{
         response.json().then((data)=>{
             if(data.error){
